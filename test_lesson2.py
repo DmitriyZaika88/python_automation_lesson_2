4d96ecf48d090cc73aa902f44c4d80c6423983ac
DmitriyZaika88 <dimonz1997@gmail.com>
2023-02-23 23:51:41 +0300

    Added files .gitignore

---
+++ test_lesson2.py
@@ -0,0 +1,30 @@
+import pytest
+from selene.support.shared import browser
+from selene import browser
+from selene import be, have
+import time
+
+
+@pytest.fixture()
+def browser_conf():
+    browser.config.hold_browser_open = True
+    browser.config.window_height = 1920
+    browser.config.window_width = 1080
+    browser.open('https://google.com')
+    return browser
+
+
+def test_valid_text_input(browser_conf):
+    browser.element('input[name=q]').should(be.blank).type('yashaka/selene').press_enter()
+    browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
+
+
+time.sleep(6)
+
+
+def test_negative_text_input(browser_conf):
+    browser.element('input[name=q]').should(be.blank).type('qwewqefgrhhrfhhfhfhdhdhfshdfhsdf').press_enter()
+    browser.element('div[class="card-section"]').should(have.text('ничего не найдено'))
+
+
+time.sleep(6)
